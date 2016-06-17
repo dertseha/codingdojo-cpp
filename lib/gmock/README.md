8c0645e45f9cabddb83b16750c8991e43f542bcc
Christian Haas <christian.haas@sevensuns.at>
2016-06-17 19:10:44 +0200

    update of gmock library adding extra test for that

--- lib/gmock/README.md
+++ lib/gmock/README.md
@@ -1,3 +1,3 @@
 ## Fused gmock Source
 
-This folder contains the fused source from the [Google Mock](https://code.google.com/p/googlemock/) library.
+This folder contains the fused source from the [Google Mock](https://github.com/google/googletest) library.
--- src/test/DummyTest.cpp
+++ src/test/DummyTest.cpp
@@ -1,13 +1,26 @@
-#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 #include <DummyClass.h>
 
 class DummyTest: public testing::Test
 {
 public:
+   class Thingie
+   {
+   public:
+      virtual ~Thingie() = default;
+
+      virtual void doStuff() = 0;
+   };
+
+   class MockedThing: public Thingie
+   {
+   public:
+      MOCK_METHOD0(doStuff, void());
+   };
 
 protected:
-    virtual void SetUp();
+   void SetUp() override;
 };
 
 
@@ -18,7 +31,16 @@ void DummyTest::SetUp()
 
 TEST_F(DummyTest, VerifyTestSystemWorks)
 {
-	DummyClass dummy(42);
+   DummyClass dummy(42);
 
-	EXPECT_EQ(42, dummy.getValue());
+   EXPECT_EQ(42, dummy.getValue());
+}
+
+TEST_F(DummyTest, VerifyMockingSystemWorks)
+{
+   MockedThing thing;
+
+   EXPECT_CALL(thing, doStuff());
+
+   thing.doStuff();
 }
